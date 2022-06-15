f4bb5b8412054512e6f94d4ab8ba099ab7aab8ad
Animesh-odoo <ayuzawa925@gmail.com>
2022-06-15 20:12:31 +0545

    docker file added

--- conceptu/urls.py
+++ conceptu/urls.py
@@ -1,10 +1,10 @@
 from django.conf import settings
-from django.conf.urls import url
+from django.conf import urls
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include
+from django.urls import include, path
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
-    url(r"^", include("products.urls")),
+    path("admin/", admin.site.urls),
+    path("", include("products.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
