5cdb9c6a280e71c877bc4eb6d6386b35a4ec3fc9
bharatshrestha925 <bharatshrestha925@gmail.com>
2022-06-16 11:27:06 +0545

    docker file added

--- hello.sh
+++ hello.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 # This is bash program to display Hello World
-echo " Hello World "
+docker-compose -f production.yml down
+docker-compose -f production.yml build
+docker-compose -f production.yml up -d
+docker-compose -f production.yml run --rm django python manage.py migrate
+docker-compose -f production.yml run --rm django python manage.py collectstatic --noinput
+docker update --restart unless-stopped $(docker ps -q)
