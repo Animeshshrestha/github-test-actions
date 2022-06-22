d7b9b6729dbc8c83558d1ba9f7b4aa8a2e9cc3e5
Anim <32807527+Animeshshrestha@users.noreply.github.com>
2022-06-22 19:48:16 +0545

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -2,7 +2,7 @@
 # This is bash program to display Hello World
 docker-compose -f production.yml down
 docker-compose -f production.yml build
-docker-compose -f production.yml up -d
+docker-compose -f production.yml up
 docker-compose -f production.yml run --rm web python manage.py migrate
 docker-compose -f production.yml run --rm web python manage.py collectstatic --noinput
 docker update --restart unless-stopped $(docker ps -q)
